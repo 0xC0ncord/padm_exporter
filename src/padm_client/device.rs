112fefbab79adc78bc3dc1508c62148313b406d5
qua3k <qua3kr@gmail.com>
2023-05-16 03:10:22 +0000

    Refactor with iterators

--- src/padm_client/device.rs
+++ src/padm_client/device.rs
@@ -1,6 +1,6 @@
 use serde_json;
 
-use crate::padm_client::variables::{Variable, is_metric, unpack_variable};
+use crate::padm_client::variables::{is_metric, unpack_variable, Variable};
 
 #[derive(Debug, Clone)]
 pub struct Device {
@@ -13,35 +13,38 @@ pub struct Device {
 pub fn load_all_from(json: &serde_json::Value) -> Result<Vec<Device>, std::io::Error> {
     let mut devices: Vec<Device> = Vec::new();
 
-    for item in json["data"].as_array().unwrap() {
-        'inner: for item in &item["attributes"].as_object() {
-            if !is_metric(item) {
+    let items: Vec<&serde_json::Map<String, serde_json::Value>> = json["data"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .filter_map(|item| item["attributes"].as_object())
+        .filter_map(|item| is_metric(&item).then_some(item))
+        .collect();
+
+    for item in items {
+        match devices
+            .iter_mut()
+            .find(|device| device.id == item["device_id"].as_i64().unwrap())
+        {
+            Some(device) => {
+                let variable = unpack_variable(item);
+                device.variables.push(variable);
                 continue;
             }
-            for device in &mut devices {
-                if device.id == item["device_id"].as_i64().unwrap() {
-                    let variable = unpack_variable(item);
-                    device.variables.push(variable);
-                    break 'inner;
-                }
-            }
-
-            let mut variables = Vec::new();
-            let variable = unpack_variable(item);
-            variables.push(variable);
-
-            let id = item["device_id"].as_i64().unwrap();
-            let name = item["device_name"].as_str().unwrap().to_string();
-            let device_type = item["device_type"].as_str().unwrap().to_string();
-
-            devices.push(Device {
-                id,
-                name,
-                device_type,
-                variables,
-            });
+            None => (),
         }
-    }
 
+        let variables = vec![unpack_variable(item)];
+        let id = item["device_id"].as_i64().unwrap();
+        let name = item["device_name"].as_str().unwrap().to_string();
+        let device_type = item["device_type"].as_str().unwrap().to_string();
+
+        devices.push(Device {
+            id,
+            name,
+            device_type,
+            variables,
+        })
+    }
     Ok(devices)
 }
