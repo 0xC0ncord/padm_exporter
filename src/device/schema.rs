86f58df77b725010b8f2240c1b536d78637028f9
0xC0ncord <me@concord.sh>
2025-07-13 01:25:29 -0400

    device: drop unused schema items

--- src/device/schema.rs
+++ src/device/schema.rs
@@ -2,8 +2,6 @@ use crate::client::variables::Variable;
 
 #[derive(Debug, Clone)]
 pub struct Device {
-    pub id: i64,
     pub name: String,
-    pub device_type: String,
     pub variables: Vec<Variable>,
 }
