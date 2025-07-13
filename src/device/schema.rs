ee8255acbc9e2cea3943662811c2231577629545
0xC0ncord <me@concord.sh>
2025-07-13 01:34:13 -0400

    Revert "device: drop unused schema items"
    This reverts commit 86f58df77b725010b8f2240c1b536d78637028f9.

--- src/device/schema.rs
+++ src/device/schema.rs
@@ -2,6 +2,8 @@ use crate::client::variables::Variable;
 
 #[derive(Debug, Clone)]
 pub struct Device {
+    pub id: i64,
     pub name: String,
+    pub device_type: String,
     pub variables: Vec<Variable>,
 }
